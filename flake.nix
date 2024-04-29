1db0c6b7b6a04e4fa02e85977fb2872b03f46e3e
kody.low <kodylow7@gmail.com>
2024-04-28 17:21:43 -0700

    feat: mutinynet support with docker and flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+  };
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        bitcoindCustom = pkgs.bitcoind.overrideAttrs (prev: {
+          src = pkgs.fetchFromGitHub {
+            owner = "benthecarman";
+            repo = "bitcoin";
+            rev = "f036909dbe288ee5b7f2c38564a3c5375255822f";
+            sha256 = "<correct-sha256>";  // You need to replace this with the actual sha256 of the commit
+          };
+        });
+      in rec {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ python3 bitcoindCustom ];
+        };
+      });
+}
